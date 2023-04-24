74d4674170194a9d98f728ed93a07c41cfdf2d09
Anuruddha_Ekanayake <apb.ekanayake.slsc@gmail.com>
2023-04-24 11:40:25 +0530

    Establishment add part done. Admin login create is ongoing

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\EstablishmentArmory;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -14,7 +15,6 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
-use App\Models\AuthReqLttrModel;
 use Spatie\Permission\Models\Role;
 
 class Controller extends BaseController
@@ -61,7 +61,9 @@ class Controller extends BaseController
     }
 
     public function create_user(){
-        return view('create_user');
+
+        $establishments = EstablishmentArmory::all();
+        return view('create_user',compact('establishments'));
     } 
 
     
@@ -147,67 +149,8 @@ class Controller extends BaseController
 
 
 
-    public function auth_req_lttr_form_func(Request $req){
-
-        $validator = Validator::make($req->all(), [
-            'req_made_location' => 'required|min:2|max:50',
-            'reason' => 'required|min:2|max:50',
-            'no_of_troops' => 'required|min:2|max:20',
-            'transport_date' => 'required|min:2|max:20',
-            'location_from' => 'required|min:2|max:50',
-            'location_to' => 'required|min:2|max:50',
-            'auth_given_by' => 'required|min:2|max:30',
-            'route' => 'required|min:2|max:100',
-            'type_of_veh' => 'required|min:2|max:20',
-            'no_of_seat' => 'required|min:1|max:20',
-            'convoy' => 'required|min:2|max:50',
-            'escort' => 'required|min:2|max:50',
-            'escort_weapon_no' => 'required|min:2|max:30',
-            'no_of_magazins' => 'required|min:1|max:20',
-            'no_of_ammo' => 'required|min:1|max:20',
-            'driver' => 'required|min:1|max:50',
-            'measures_taken' => 'required|min:1|max:50',
-            'ref_of_ltr' => 'required|min:1|max:50',
-            'att' => 'required|min:1|max:100',
-        ],
-        [
-            'reason' => 'Reason is Needed',
-        ]);
-
-        if ($validator->fails()) { 
-            return response()->json(['status' => 1 , 'error' => $validator->errors()->toArray()]);
-        }
-        elseif ($validator->passes()) {
-            
-            $auth_req_lttr = new AuthReqLttrModel();
-            $auth_req_lttr->request_made_by = $req->req_made_location;
-            $auth_req_lttr->reason = $req->reason;
-            $auth_req_lttr->no_of_troops = $req->no_of_troops;
-            $auth_req_lttr->transport_date = $req->transport_date;
-            $auth_req_lttr->location_from = $req->location_from;
-            $auth_req_lttr->location_to = $req->location_to;
-            $auth_req_lttr->auth_given_by = $req->auth_given_by;
-            $auth_req_lttr->route = $req->route;
-            $auth_req_lttr->type_of_veh = $req->type_of_veh;
-            $auth_req_lttr->no_of_seat = $req->no_of_seat;
-            $auth_req_lttr->convoy_comd = $req->convoy;
-            $auth_req_lttr->escort = $req->escort;
-            $auth_req_lttr->escort_weapon_no = $req->escort_weapon_no;
-            $auth_req_lttr->no_of_magazins = $req->no_of_magazins;
-            $auth_req_lttr->no_of_ammo = $req->no_of_ammo;
-            $auth_req_lttr->driver = $req->driver;
-            $auth_req_lttr->measures = $req->measures_taken;
-            $auth_req_lttr->ref_of_ltr = $req->ref_of_ltr;
-            $auth_req_lttr->attachment = $req->att;
-            $auth_req_lttr->added_by = auth()->user()->username;
-            $auth_req_lttr->ip = $_SERVER['REMOTE_ADDR'];
-            $auth_req_lttr->save();
-
-            return response()->json(['status' => 0 , 'msg' => 'Registration Success' ]);
-        }
-        else{
-            echo 'System Error';
-        }
+    public function add_establishment_func(Request $req){
+        dd('es');
     }
     
 
@@ -249,15 +192,6 @@ class Controller extends BaseController
     //// DATA TABLE ////
 
 
-
-
-
-    // public function auth_req_lttr_form_func(Request $req){
-
-
-        
-    // }
-    
 
 
 
--- app/Models/EstablishmentArmory.php
+++ app/Models/EstablishmentArmory.php
@@ -4,11 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class AuthReqLttrModel extends Model
+class EstablishmentArmory extends Model
 {
     use HasFactory;
-
-    protected $table = 'auth_req_lttrs';
-
+    protected $guarded = [''];
+    use SoftDeletes;
 }
